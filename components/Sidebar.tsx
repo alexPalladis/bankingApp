f93a825a8b59635bce71adff36052db0c66c31a5
alex_Palladis <alehandro4443@gmail.com>
2024-11-25 22:10:12 +0200

    Finalize the App

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import Footer from './Footer';
+import PlaidLink from './PlaidLink';
 
 const Sidebar = ({user}:SiderbarProps) => {
     const pathName = usePathname();
@@ -37,7 +38,9 @@ const Sidebar = ({user}:SiderbarProps) => {
                         </div>
                         <p className={cn('sidebar-label',{'!text-white' :isActive})}>{item.label}</p>
                     </Link>
-            )})}            
+            )})} 
+
+            <PlaidLink user={user}/>           
         </nav>
 
         <Footer user={user}/>
